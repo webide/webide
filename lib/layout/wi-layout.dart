9cbfcd778d43d2b3ef953c7c8fd4799b3e13e6ae
Ravi Teja Gudapati <tejainece@gmail.com>
2014-05-07 23:31:06 +0200

    Implemented flex layout in wi-layout-container

---
+++ lib/layout/wi-layout.dart
@@ -0,0 +1,16 @@
+library webide.layout;
+
+import 'package:polymer/polymer.dart';
+
+@CustomTag('wi-layout')
+class WiLayout extends PolymerElement {
+  WiLayout.created() : super.created() {
+    
+  }
+  
+  @override
+  void enteredView() {
+    super.enteredView();
+
+  }
+}
